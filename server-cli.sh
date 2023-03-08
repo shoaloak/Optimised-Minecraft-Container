225c6f4e9cc904a9ef55eb30b9dcb56b25797cd2
Axel Koolhaas <shoaloak@quartz.lan>
2023-03-08 21:51:45 +0100

    added shutdown

--- server-cli.sh
+++ server-cli.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 readonly CONTAINER_ID=`podman ps | grep minecraft | awk '$1 { print $1 }'`
+readonly RCON_CMD="podman exec -it $CONTAINER_ID mcrcon/mcrcon -p ImInsideTheContainer"
 
 PS3='What would you like to do: '
-options=("Shell" "RCON" "Quit")
+options=("Shell" "RCON" "Shutdown" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -14,7 +15,16 @@ do
             ;;
         "RCON")
             echo "Connecting with RCON..."
-            podman exec -it $CONTAINER_ID mcrcon/mcrcon -p ImInsideTheContainer
+            $RCON_CMD
+            break
+            ;;
+        "Shutdown")
+            echo "Stopping server..."
+            $RCON_CMD "say Server is going down in 5 seconds"
+            sleep 5
+            $RCON_CMD save-all
+            sleep 1
+            $RCON_CMD stop
             break
             ;;
         "Quit")
